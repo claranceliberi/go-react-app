494d467342d392e6f74bf0e20b6ea7e7fc7d2dae
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2023-01-18 23:46:30 +0200

    gorm connection

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
+
+	_, err := gorm.Open(mysql.Open("liberi:liberi@127.0.0.1:5972/go-react-app"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to the database")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
